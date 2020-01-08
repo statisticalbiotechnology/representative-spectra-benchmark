e22f015d4554a0d8fd0d5ca2a2a216a32d38a87a
percolator <lukas.kall@scilifelab.se>
2020-01-08 14:00:20 +0100

    Still debugging

---
+++ main.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python src/plot_cluster.py 01650b_BA5-TUM_first_pool_75_01_01-3xHCD-1h-R2.mzML MaRaCluster.clusters_p5.tsv 40567
--- src/plot_cluster.py
+++ src/plot_cluster.py
@@ -1,3 +1,4 @@
+import numpy as np
 import spectrum_utils.plot as sup
 import spectrum_utils.spectrum as sus
 from pyteomics import mgf
@@ -5,7 +6,7 @@ import sys
 import pymzml
 
 
-def plot_spectrum(identifier,precursor_mz,precursor_charge, mz,intensity,peptide):
+def plot_spectrum(identifier,precursor_mz,precursor_charge, mz,intensity,retention_time,peptide):
 #identifier = spectrum_dict['params']['title']
 #precursor_mz = spectrum_dict['params']['pepmass'][0]
 #precursor_charge = spectrum_dict['params']['charge'][0]
@@ -13,6 +14,10 @@ def plot_spectrum(identifier,precursor_mz,precursor_charge, mz,intensity,peptide
 #intensity = spectrum_dict['intensity array']
 #retention_time = float(spectrum_dict['params']['rtinseconds'])
 #peptide = 'WNQLQAFWGTGK'
+    print(type(mz))
+    print(type(mz[0]))
+    print(np.asarray(mz, np.float32))
+    print(np.asarray(intensity, np.float32))
 
 # Create the MS/MS spectrum.
     spectrum = sus.MsmsSpectrum(
@@ -37,13 +42,13 @@ def main(mzml_file,cluster_file,scan):
     scans, tmp_scans = set(), set()
     with open(cluster_file) as cluster_def:
         for line in cluster_def:
-            if len(line)==0:
-                if searchscan in tmp_scans:
+            if line.isspace():
+                if scan in tmp_scans:
                     scans.update(tmp_scans)
                 tmp_scans = set()
             else:
-                words = line.split('t')
-                scans.add(words[1])
+                words = line.split('\t')
+                tmp_scans.add(int(words[1]))
     print("Plotting cluster of spectra with the following scans ", scans, file=sys.stderr)
     run = pymzml.run.Reader(mzml_file)
     for n, spec in enumerate(run):
@@ -53,8 +58,12 @@ def main(mzml_file,cluster_file,scan):
                     spec.ID, ms_level=spec.ms_level, scan_time=spec.scan_time_in_minutes()
                     )
             )
-            precursors = spec.precursors()
-            plot_spectrum(spec.ID,precursors[0]["mz"],precursors[0]["charge"], spec.mz,spec.i,"SLEGSSDTALPLRR")
+            #try:
+            #    spec._selected_precursors
+            #except AttributeError:
+            #    spec._selected_precursors = None
+            precursors = spec.selected_precursors
+            plot_spectrum(spec.ID,precursors[0]["mz"],precursors[0]["charge"], spec.mz,spec.i,spec.scan_time_in_minutes(),"SLEGSSDTALPLRR")
     print("Parsed {0} spectra from file {1}".format(n, mzml_file))
 
 
@@ -65,4 +74,4 @@ if __name__ == "__main__":
     mzml_file = sys.argv[1]
     cluster_file = sys.argv[2]
     scan = sys.argv[3]
-    main(mzml_file,cluster_file,scan)
+    main(mzml_file,cluster_file,int(scan))
