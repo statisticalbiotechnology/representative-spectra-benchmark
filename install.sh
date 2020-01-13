c6633a0bef16821ecbfe64c8e721221351844b71
Lukas Käll <lukas.kall@gmail.com>
2020-01-13 15:54:19 +0100

    Adding install script

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-
+conda create --name specpride python=3
 conda install -c conda-forge -c bioconda -c defaults -y -q --name specpride --file requirements.txt
 conda activate specpride
+
+mkdir -p data
+cd data
+curl ftp://ftp.pride.ebi.ac.uk/pride/data/proteogenomics/projects/eubic-2020/01650b_BA5-TUM_first_pool_75_01_01-3xHCD-1h-R2.mzML
+curl ftp://ftp.pride.ebi.ac.uk/pride/data/proteogenomics/projects/eubic-2020/msms.txt
+curl ftp://ftp.pride.ebi.ac.uk/pride/data/proteogenomics/projects/eubic-2020/peptides.txt
+cd ..
