4cbfc33ec1fc4024251b5c31acb927ac89ee4fea
Lukas Käll <lukas.kall@gmail.com>
2020-01-15 16:05:20 +0100

    Broken.

--- src/benchmark.py
+++ src/benchmark.py
@@ -8,17 +8,8 @@ import metrics as mx
 def test_method_metric(member_file,representative_file,metric=mx.cos_dist):
     with mgf.mgf(member_file) as memb_reader, mgf.mgf(representative_file) as rep_reader:
         m_spec = memb_reader.next()
-        prev_clid = "none"
-        while (r_spec = rep_reader.next()):
-            m_specs = []
-            curr_clid = prev_clid
-            while prev_clid==curr_clid:
-                m_specs += m_spec
-                m_spec = memb_reader.next()
-                curr_clid = m_spec["title"]
-                print(curr_clid)
-            prev_clid = curr_clid
-            print("xxx")
+        m_specs = []
+        for r_spec in rep_reader:
 
 if __name__ == "__main__":
     test_method_metric(
