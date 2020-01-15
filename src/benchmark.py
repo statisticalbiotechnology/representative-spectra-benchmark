4d1165f58bd20563c7629bcc61972f8562dff285
Lukas Käll <lukas.kall@gmail.com>
2020-01-15 13:41:04 +0100

    Added code for cosine distance

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,6 +1,42 @@
+import numpy as np
 import sys
 import spectrum_utils.spectrum as sus
 from pyteomics import mgf, parser
+from scipy.stats import binned_statistic
+
+
+mz_unit = 1.000508 # Space in Th between fragments
+mz_space = mz_unit*.005 # Resolution approximately 0.005 Da
+
+def bin_proc(spectrum, mz_space, max_mz):
+    bins =  np.arange(-mz_space/2., max_mz, mz_space)
+    # bins = np.linspace(-mz_space/2., max_mz, num=xxx)
+    dig_spec, _, __ = binned_statistic(spectrum.mz, spectrum.intensity,
+        statistic='sum', bins=bins)
+    return dig_spec
+
+
+def cos_dist(representative_spectrum, cluster_member):
+    max_mz = max(representative_spectrum.mz[-1],cluster_member.mz[-1])
+    discrete_a = bin_proc(representative_spectrum, mz_space, max_mz)
+    discrete_b = bin_proc(cluster_member, mz_space, max_mz)
+    a = np.dot(discrete_a, discrete_a)
+    b = np.dot(discrete_b, discrete_b)
+    ab = np.dot(discrete_a, discrete_b)
+    if a==0. or b==0.:
+        return 0.
+    else:
+        return ab/np.sqrt(a*b)
+
+
+def average_cos_dist(representative_spectrum, cluster_members):
+    sum_dist = 0.0
+    for member in cluster_members:
+        sum_dist += cos_dist(representative_spectrum,member)
+    if len(cluster_members)>0:
+        return sum_dist/float(len(cluster_members))
+    else:
+        return 0.0
 
 def fraction_of_by(peptide_seq, precursor_mz, precursor_charge,mz, intensity):
     if not parser.fast_valid(peptide_seq):
