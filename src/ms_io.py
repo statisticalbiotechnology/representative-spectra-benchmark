31cdaf4c96f97ee8fd553e342a49fdee2e5073cb
Wout <wout@bittremieux.be>
2020-01-28 09:51:09 -0800

    Correct identifier from spectra-cluster assignments

--- src/ms_io.py
+++ src/ms_io.py
@@ -255,11 +255,12 @@ def _read_clusters_spectracluster(filename: str) -> Dict[str, int]:
             if line.startswith('=Cluster='):
                 cluster_i += 1
             elif line.startswith('SPEC'):
-                fn = line[line.find('#file') + len('#file'):line.find('#id')]
+                fn = line[line.find('#file') + len('#file') + 1:
+                          line.find('#id')]
                 scan = line[line.find('#id=index=') + len('#id=index='):
                             line.find('#title')]
-                clusters[f'{os.path.splitext(fn)[0]}:scan:{scan}'] = \
-                    cluster_i
+                clusters[f'{os.path.splitext(os.path.basename(fn))[0]}:scan:'
+                         f'{scan}'] = cluster_i
                 progress_bar.update(1)
         return clusters
 
